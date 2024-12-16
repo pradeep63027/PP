0ed9e98451a4cc44e8067f0ba9f8317669330e56
pradeep <mekalapradeepkumar099@gmail.com>
2024-12-16 15:43:55 +0530

    searchi

--- src/Components/Profile/index.js
+++ src/Components/Profile/index.js
@@ -1,12 +1,13 @@
 const Profile = (props) => {
     const {userDetails} = props
-    const {name,imageUrl,role} = userDetails
+    const {name,imageUrl,role,contact} = userDetails
 
     return(
         <li>
             <img src={imageUrl} alt="p"/>
             <h1>{name}</h1>
             <p>{role}</p>
+            <p>{contact}</p>
 
         </li>
     )
